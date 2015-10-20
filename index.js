65cb65637233f352a6074809c54cd3b4e3c87624
Adrian Stainforth <adrian@morsedigtial.com>
2015-10-20 11:44:44 +0100

    minor tweak
    Signed-off-by: Adrian Stainforth <adrian@morsedigtial.com>

--- index.js
+++ index.js
@@ -74,8 +74,11 @@ var DataManager = (function () {
         if (_.contains(keys, k) && !_.isNull(item)) {
           var dateFmt = new DateFormatter(v);
           item[k] = dateFmt.getDate();
-          var key = k + "Df";
-          item[key] = dateFmt;
+          var _key2 = k + "Df";
+          item[_key2] = dateFmt;
+        } else {
+          item[k] = v;
+          item[key] = "";
         }
       });
 
@@ -346,9 +349,9 @@ var DataManager = (function () {
 
                 return all.search(regex) > -1;
               } else {
-                var key = keys[0];
-                if (d.has(key)) {
-                  var value = d.get(key);
+                var _key3 = keys[0];
+                if (d.has(_key3)) {
+                  var value = d.get(_key3);
                   return String(value).search(regex) > -1;
                 } else {
                   return false;
